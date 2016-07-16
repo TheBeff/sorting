bf6d6a80156c8bbdf30d5b1a2be4ec1c8519eb8e
TheBeff <thebeff@gmail.com>
2016-07-16 17:54:02 -0400

    Added Jasmine Spies
    took way too long >: [

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,23 @@
-function bubbleSort(array){
+var swap = function(array, i){
+	var temp = array[i];
+	array[i] = array[i+1];
+	array[i+1] = temp;
+};
 
+var compare = function(array, i){
+	return array[i] > array[i+1];
+};
+
+var bubbleSort = function(array){
 
 	for(var j = 0; j < array.length; j++){ 
 		for(var i = 0; i < array.length-j; i++){
-			if(array[i] > array [i+1]){
-				var temp = array[i];
-				array[i] = array[i+1];
-				array[i+1] = temp;
+			if(compare(array, i)){
+				swap(array, i);
 			}
 		}
 	}
 
 	return array;
 
-}
+};
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,15 @@
 describe('Bubble Sort', function(){
   
+  beforeEach(function(){
+  	spyOn(window, 'swap').and.callThrough();
+  	spyOn(window, 'compare').and.callThrough();
+  });
+
+  afterEach(function(){
+  	console.log('Swaps performed: ' + swap.calls.count());
+  	console.log('Comparisons made: ' + compare.calls.count());
+  });
+
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
@@ -12,8 +22,9 @@ describe('Bubble Sort', function(){
   	expect( bubbleSort([1, 7, 5, 6, 2]) ).toEqual( [1, 2, 5, 6, 7] );
   });
 
-   it('handles an array of length > 1', function(){
+  it('handles an array of length > 1', function(){
   	expect( bubbleSort([98, 53, 5, 6, 2, 27, 45, 3]) ).toEqual( [2, 3, 5, 6, 27, 45, 53, 98] );
   });
+
 
 });
